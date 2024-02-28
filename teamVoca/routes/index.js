fcedb57cc6b4c81dead450f7bb8fe7c654128892
yangtory <wjdduscldrn@naver.com>
2024-02-28 12:40:53 +0900

    수정

--- teamVoca/routes/index.js
+++ teamVoca/routes/index.js
@@ -1,9 +1,9 @@
-import express from 'express'
-const router = express.Router()
+import express from "express";
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', async (req, res, next) => {
-  res.render('index', { title: 'Hello callor.com Express' })
-})
+router.get("/", async (req, res, next) => {
+  res.render("index");
+});
 
-export default router
+export default router;
