ee9dc0a45a7e1235dcdcdc40c415f17ccd8c3cb2
yangtory <wjdduscldrn@naver.com>
2024-03-04 23:33:44 +0900

    오류잔치

--- teamVoca/module/naver_api.js
+++ teamVoca/module/naver_api.js
@@ -1,21 +1,30 @@
-import { NAVER_CLIENT_ID, NAVER_CLIENT_SECRET, NAVER_BOOK_URL } from '../config/naver_secret.js';
-
 const getWords = async (search) => {
-    const naverFetchOption = {
-        method: 'GET',
-        headers: {
-            'X-Naver-Client-Id': NAVER_CLIENT_ID,
-            'X-Naver-Client-Secret': NAVER_CLIENT_SECRET,
-        },
+    const KOGPT_URL = 'https://dapi.kakao.com/v2/search/web';
+
+    const prompt = `${search}`;
+
+    const body = {
+        query: prompt,
+        // max_tokens: 20, // 생성할 결과의 최대 토큰 수
+        // temparature: 1, // 온도 설정
+        // top_p: 1, // 상위 확률 결정
+        // n: 1, // 생성할 결과 수(쿼터 차감)
     };
-    let queryString = `${NAVER_BOOK_URL}?query=${search}&display=10`;
-    let res = null;
-    try {
-        res = await fetch(queryString, naverFetchOption);
-        return (await response.json()).item;
-    } catch (error) {
-        return res.json(error);
-    }
+
+    // kogpt API 호출
+    const response = await fetch(KOGPT_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            Authorization: 'KakaoAK ' + '7dbc1d9c6ab536a62d349b555586eb97', // API Key
+        },
+
+        body: JSON.stringify(body),
+    });
+
+    return await response.json();
+
+    // console.log(json);
 };
 
 export { getWords };
--- teamVoca/routes/quiz.js
+++ teamVoca/routes/quiz.js
@@ -4,10 +4,14 @@ import { getWords } from '../module/naver_api.js';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
+    // return res.json(words);
+    return res.render('quiz/quizmain');
+});
+
+router.get('/kakao', async (req, res) => {
     const search = req.query.search;
     const words = await getWords(search);
-    // return res.json(words);
+    return res.json(words);
     return res.render('quiz/quizmain', { WORDS: words });
-    // return res.render('quiz/quizmain');
 });
 export default router;
