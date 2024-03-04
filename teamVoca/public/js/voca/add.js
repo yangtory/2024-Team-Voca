848aedadd5feeb16dbc2c6b9e9394adc1120a272
Yeonsu1027 <rito1205@naver.com>
2024-03-04 23:22:11 +0900

    js 유효성검사,수정기능 수정

--- teamVoca/public/js/voca/add.js
+++ teamVoca/public/js/voca/add.js
@@ -1,49 +1,66 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const p_false = document.querySelector("input.false");
+//   const p_true = document.querySelector("input.true");
+
+//   // 체크되면 다른쪽 체크풀리게
+//   p_false.addEventListener("change", () => {
+//     if (p_false.checked) {
+//       p_true.checked = false;
+//     }
+//   });
+
+//   p_true.addEventListener("change", () => {
+//     if (p_true.checked) {
+//       p_false.checked = false;
+//     }
+//   });
+// });
+// // 유효성검사
+// //이름안적거나 넘으면 버튼비활성화가 나을듯 
+// // disabled false, true (true 가 안보이는거)
+// document.addEventListener("DOMContentLoaded", () => {
+//   const name = document.querySelector("input.voca_name");
+//   const btn = document.querySelector("button.make_voca");
+
+//   name.addEventListener("input", () => {
+//     if (name.value === "" || name.value.length > 20) {
+//       btn.disabled = true;
+//     } else {
+//       btn.disabled = false;
+//     }
+//   });
+// });
+
+
 document.addEventListener("DOMContentLoaded", () => {
+  const name = document.querySelector("input.voca_name");
   const p_false = document.querySelector("input.false");
   const p_true = document.querySelector("input.true");
+  const btn = document.querySelector("button.make_voca");
+
+  const checkInputs = () => {
+    if (name.value === "" || name.value.length > 20) {
+      btn.disabled = true;
+    } else {
+      btn.disabled = false;
+    }
+  }
 
   // 체크되면 다른쪽 체크풀리게
   p_false.addEventListener("change", () => {
     if (p_false.checked) {
       p_true.checked = false;
     }
+    checkInputs();
   });
 
   p_true.addEventListener("change", () => {
     if (p_true.checked) {
       p_false.checked = false;
     }
+    checkInputs();
   });
+
+  name.addEventListener("input", checkInputs);
 });
 
-// 이름 안쓰면 버튼 생성안되게.. 미완성
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const name = document.querySelector("input.voca_name");
-//   const btn = document.querySelector("button.make_voca");
-//   // const form = document.querySelector("form.add_words");
-//   btn.addEventListener("click", () => {
-//     if (name.value === "") {
-//       alert("단어장 이름을 입력해주세요!");
-//       return false;
-//       // 생성되고있음..
-//     } else if (name.value.length > 20) {
-//       alert("20자까지 작성가능합니다.");
-//       return false;
-//     }
-//   });
-// });
-// 안되는데 이름안적거나 넘으면 버튼비활성화가 나을듯 // 아직안됨.
-//disabled false, true
-document.addEventListener("DOMContentLoaded", () => {
-  const name = document.querySelector("input.voca_name");
-  const btn = document.querySelector("button.make_voca");
-  // const form = document.querySelector("form.add_words");
-  btn.addEventListener("click", () => {
-    if (name.value === "") {
-      btn.disabled = true; // true 가 안보이는건가
-    } else if (name.value.length > 20) {
-      btn.disabled = true;
-    }
-  });
-});
--- teamVoca/public/js/voca/add_words.js
+++ teamVoca/public/js/voca/add_words.js
@@ -1,24 +1,19 @@
-// 좀이상함
+
 document.addEventListener("DOMContentLoaded",()=>{
+    const word = document.querySelector("input.word");
+    const mean = document.querySelector("input.mean");
+    const btn = document.querySelector("button.add");
 
-    const add = document.querySelector("button.add");
-
-    const input_check = async () =>{
-        const word = document.querySelector("input.word");
-        const mean = document.querySelector("input.mean");
-        if(word.value === "") {
-            word.focus();
-            alert("영어단어를 입력해주세요")
-            return false;
-        } else if(mean.value === ""){
-            mean.focus();
-            alert("단어 뜻을 입력해주세요")
-            return false;
+    const input =()=> {
+        // 둘다 적어야 활성화
+        if(word.value !== "" && mean.value !== "") {
+            btn.disabled = false;
+        } else {
+            btn.disabled = true;
         }
-
     }
-    add.addEventListener("click",input_check)
 
-
-  
-})
+    // 단어랑 뜻 적었으면..
+    word.addEventListener("input", input);
+    mean.addEventListener("input", input);
+});
