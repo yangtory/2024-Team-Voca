bb181dd121d85da51cc5c0490026c5339d85ad07
yangtory <wjdduscldrn@naver.com>
2024-03-05 23:52:07 +0900

    프로필수정

--- teamVoca/public/js/setting/main.js
+++ teamVoca/public/js/setting/main.js
@@ -1,35 +1,36 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const menu = document.querySelector("ul");
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('ul');
+    const update = document.querySelector('input.update');
 
-  menu?.addEventListener("click", (e) => {
-    const target = e.target;
-    if (target.className === "logout") {
-      alert("로그아웃 되었습니다.");
-      document.location.href = "setting/logout";
-    }
-  });
-  const toggle = document.querySelector("input.check");
-  const toggleActive = localStorage.getItem("active");
-  // const toggleState = (toggleOn) => {
-  toggle.checked = toggleActive === "true"; // 문자열 "true"를 받으면 true로 변환
-  // };
-  toggle.addEventListener("click", async (e) => {
-    // toggle.classList.toggle("on");
-    // if (toggle.classList.contains("on")) {
-    console.log(toggle.checked);
+    menu?.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.className === 'logout') {
+            alert('로그아웃 되었습니다.');
+            document.location.href = 'setting/logout';
+        }
+    });
+    const toggle = document.querySelector('input.check');
+    const toggleActive = localStorage.getItem('active');
 
-    if (toggle.checked) {
-      localStorage.setItem("active", "true");
-      alert("비공개 전환 후 해제하면 모든 단어장이 공개됩니다◠‿◠");
-      document.location.href = "setting/toggle";
-    } else {
-      console.log("false");
-      localStorage.setItem("active", "false");
-      alert("모든 단어장이 공개되었습니다◝(・▿・)◜");
-      document.location.href = "setting/toggleoff";
-    }
-  });
-  // window.onload = () => {
-  //   toggleState(toggleActive === "true");
-  // };
+    toggle.checked = toggleActive === 'true'; // 문자열 "true"를 받으면 true로 변환
+
+    toggle.addEventListener('click', async (e) => {
+        console.log(toggle.checked);
+
+        if (toggle.checked) {
+            localStorage.setItem('active', 'true');
+            alert('비공개 전환 후 해제하면 모든 단어장이 공개됩니다◠‿◠');
+            document.location.href = 'setting/toggle';
+        } else {
+            console.log('false');
+            localStorage.setItem('active', 'false');
+            alert('모든 단어장이 공개되었습니다◝(・▿・)◜');
+            document.location.href = 'setting/toggleoff';
+        }
+    });
+
+    update.addEventListener('click', () => {
+        const m_id = update.dataset.id;
+        document.location.href = `setting/${m_id}/update`;
+    });
 });
