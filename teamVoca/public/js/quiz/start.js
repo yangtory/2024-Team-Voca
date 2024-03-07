f4d63353ef4a68d8c05e2c1fecf55e9f6dadebec
yangtory <wjdduscldrn@naver.com>
2024-03-08 00:10:14 +0900

    퀴즈

--- teamVoca/public/js/quiz/start.js
+++ teamVoca/public/js/quiz/start.js
@@ -5,27 +5,45 @@
 2. res_box 의 값과 비교해서 정답 여부 확인
 3. next 버튼을 누르면 오답이 되고 다음 단어가 나온다
 */
-const quiz_event = async () => {
-  const quiz_section = document.querySelector("section.body");
-  const comment = document.querySelector("p.comment");
-  const word_box = document.querySelector("input.word");
-  const res_box = document.querySelector("input.res");
-  const correct_btn = document.querySelector("input.correct");
-  const btn_box = document.querySelector("div.btn.next");
 
-  // v_seq 가져오고 단어 리스트 json 받아오기
-  const w_vseq = quiz_section.dataset.vseq;
-  console.log(v_seq);
-  const res = await fetch(`/quiz/start/${w_vseq}`);
-  const word = await res.json();
+document.addEventListener('DOMContentLoaded', () => {
+    const word_box = document.querySelector('p.word');
+    const quiz_section = document.querySelector('section.body');
+    const comment = document.querySelector('p.comment');
+    const res_box = document.querySelector('input.res');
+    const correct_btn = document.querySelector('input.correct');
+    const btn_box = document.querySelector('div.btn.next');
 
-  word_box.value = word.w_word;
-  console.log(word_box.value);
+    const getWord = async () => {
+        // v_seq 가져오고 단어 리스트 json 받아오기
+        const w_vseq = quiz_section.dataset.vseq;
+        const response = await fetch(`/quiz/getword/${w_vseq}`);
+        const data = await response.json();
 
-  // json 에 있는 단어들 word_box에 띄우기
-};
+        if (data.message) {
+            alert(data.message);
+        } else {
+            word_box.innerText = data.WORD.w_mean;
+        }
+    };
 
-document.addEventListener("DOMContentLoaded", () => {
-  quiz_event();
-  //   word_box.addEventListener("load", quiz_event);
+    const nextWord = async () => {
+        const w_vseq = quiz_section.dataset.vseq;
+        const response = await fetch(`/quiz/nextword/${w_vseq}`);
+        const data = await response.json();
+
+        if (data.message) {
+            alert(data.message);
+        } else {
+            getWord();
+        }
+    };
+
+    getWord();
+    btn_box.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target.className === 'next') {
+            nextWord();
+        }
+    });
 });
