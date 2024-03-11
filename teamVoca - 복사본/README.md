a81e6f7c771d14f4efd5f17cc414410c3af5763f
n96js <n96js@naver.com>
2024-03-11 23:13:14 +0900

    수정완료

--- teamVoca - 복사본/README.md
+++
@@ -1,3 +0,0 @@
-- sequelize-auto -o "./models" -d vocadb -h localhost -u root -x '!Biz8080' -e mysql -l esm
-
-- npm install --save @google-cloud/translate
--- teamVoca - 복사본/public/js/login.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const join = document.querySelector("input.join");
-
-  join.addEventListener("click", () => {
-    return (document.location.href = "/join");
-  });
-});
