0d02f379c3c7c10813c5fd83ebb93021139371d7
yangtory <wjdduscldrn@naver.com>
2024-03-11 22:43:20 +0900

    tuto

--- teamVoca/routes/tuto.js
+++ teamVoca/routes/tuto.js
@@ -1,6 +1,8 @@
 import express from 'express';
 const router = express.Router();
 
-router.get('/', async (req, res) => {});
+router.get('/', async (req, res) => {
+    return res.render('tuto/add');
+});
 
 export default router;
